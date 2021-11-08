788878acd8cb7c0e1fad1f24c02d8c83b7a31a58
苏凌峰 <511045477@qq.com>
2021-11-08 16:38:43 +0800

    注释增加

--- slf-test/src/main/java/slf/demo.java
+++ slf-test/src/main/java/slf/demo.java
@@ -1,5 +1,6 @@
 package slf;
 
+import org.springframework.beans.factory.FactoryBean;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import slfEditor.Customer;
 
@@ -7,9 +8,13 @@ public class demo {
 
 	public static void main(String[] args) throws Exception {
 		ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-
-		A a = (A) classPathXmlApplicationContext.getBean(A.class);
-		B b = classPathXmlApplicationContext.getBean(B.class);
+		A a12 = (A)classPathXmlApplicationContext.getBean("myFactoryBean");
+		A a13 = (A)classPathXmlApplicationContext.getBean("myFactoryBean");
+		FactoryBean factoryBean = (FactoryBean)classPathXmlApplicationContext.getBean("&myFactoryBean");
+		System.out.println(a12==a13);
+		System.out.println(factoryBean);
+		//A a = (A) classPathXmlApplicationContext.getBean(A.class);
+		//B b = classPathXmlApplicationContext.getBean(B.class);
 		/*		MyFactoryBean myFactoryBean = (MyFactoryBean) classPathXmlApplicationContext.getBean(MyFactoryBean.class);
 		A a = (A) classPathXmlApplicationContext.getBean("myFactoryBean");
 		System.out.println(a);
@@ -21,7 +26,7 @@ public class demo {
 		/*Customer c = classPathXmlApplicationContext.getBean(Customer.class);
 		System.out.println(c);*/
 
-		System.out.println(a.getB() == b);
+		//System.out.println(a.getB() == b);
 		System.out.println(666);
 
 
